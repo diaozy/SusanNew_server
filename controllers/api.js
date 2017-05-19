9970c5524575f8fc6be2d1c8f1cd36a3f8c74c0e
diaozy <diaozhongying@scs.com>
2017-05-19 17:37:10 +0800

    up

--- controllers/api.js
+++ controllers/api.js
@@ -4,25 +4,38 @@ var sbs1 =
 {
 
 "100001": {
-mp3Url:"	http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E5%AD%A6%E7%94%9F%E7%94%A8%E4%B9%A6%E9%9F%B3%E9%A2%91/A001Introduction%20.MP3",
-album: { 
-picUrl: "../../images/sbs1.jpg",
-name: "国际朗文英语第1册",
- }, 
-artists:"国际朗文英语" , 
- name: "A001Introduction ...",
-zh: ""
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E4%B8%87%E4%BA%8B%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语万事小故事",
+artists:"Susan英语学堂" 
 },
+
 "100002": {
-mp3Url:"	http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E5%AD%A6%E7%94%9F%E7%94%A8%E4%B9%A6%E9%9F%B3%E9%A2%91/P001_Chapter%2001_Vocabulary%20Preview.mp3",
-album: { 
-picUrl: "../../images/sbs1.jpg",
-name: "国际朗文英语第1册",
- }, 
-artists:"国际朗文英语" , 
- name: "P001_Chapter 01_Vocabular...",
-zh: ""
-}};
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E4%BA%BA%E7%89%A9%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语人物小故事",
+artists:"Susan英语学堂" 
+},
+
+"100003": {
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E5%8A%A8%E7%89%A9%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语动物小故事",
+artists:"Susan英语学堂" 
+},
+
+"100004": {
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E5%93%B2%E7%90%86%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语哲理小故事",
+artists:"Susan英语学堂" , 
+ name: "英语哲理小故事"
+},
+
+
+
+
+};
 
 
 module.exports = {
@@ -33,13 +46,4 @@ module.exports = {
     },
 
 
-    'POST /api/products': async (ctx, next) => {
-        var p = {
-            name: ctx.request.body.name,
-            price: ctx.request.body.price
-        };
-        products.push(p);
-        ctx.response.type = 'application/json';
-        ctx.response.body = p;
-    }
 };
