0c20631a0d78b82c4abb1a11b3aeb485bd51d379
diaozy <diaozhongying@scs.com>
2017-05-23 16:54:38 +0800

    up

--- controllers/api.js
+++ controllers/api.js
@@ -1,27 +1,115 @@
 
 
+var books =
+{
+
+
+"111": {
+bookID:"story3",
+picUrl: "http://susanlistening-1253455298.file.myqcloud.com/images/book3.jpg",
+name: "英语小故事（3年级）",
+artists:"Susan英语学堂" ,
+},
+
+"112": {
+bookID:"story4",
+picUrl: "http://susanlistening-1253455298.file.myqcloud.com/images/book4.jpg",
+name: "英语小故事（4年级）",
+artists:"Susan英语学堂" ,
+},
+
+};
+
+
+var backup =
+{
+
+"101": {
+bookID:"sbs1",
+picUrl: "http://susanlistening-1253455298.file.myqcloud.com/images/book1.jpg",
+name: "Side by Side国际朗文英语1",
+artists:"SBS" ,
+},
+
+"102": {
+bookID:"sbs1e",
+picUrl: "http://susanlistening-1253455298.file.myqcloud.com/images/book2.jpg",
+name: "Side by Side国际朗文练习册1",
+artists:"SBS" ,
+},
+
+};
+
+
 var sbs1 = 
+{
+
+"100001": {
+mp3Url:"http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E5%AD%A6%E7%94%9F%E7%94%A8%E4%B9%A6%E9%9F%B3%E9%A2%91/A001Introduction%20.MP3",
+picUrl: "../../images/sbs1.jpg",
+name: "A001Introduction ...",
+artists:"SBS" ,
+album:"国际朗文英语第1册"
+},
+
+"100002": {
+mp3Url:"	http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E5%AD%A6%E7%94%9F%E7%94%A8%E4%B9%A6%E9%9F%B3%E9%A2%91/P001_Chapter%2001_Vocabulary%20Preview.mp3",
+picUrl: "../../images/sbs1.jpg",
+name: "P001_Chapter 01_Vocabular...",
+artists:"SBS" ,
+album:"国际朗文英语第1册"
+},
+
+};
+
+var sbs1e = 
+{
+
+"100001": {
+mp3Url:"	http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E7%BB%83%E4%B9%A0%E5%86%8C%E9%9F%B3%E9%A2%91/A001Introduction%20.MP3",
+picUrl: "../../images/sbs1e.jpg",
+name: "A001Introduction ...",
+artists:"SBS" ,
+album:"国际朗文英语第1册练习"
+},
+
+"100002": {
+mp3Url:"	http://	susanlistening-1253455298.file.myqcloud.com/Side%20by%20Side/SBS1MP3/%E7%AC%AC1%E5%86%8C%20%E7%BB%83%E4%B9%A0%E5%86%8C%E9%9F%B3%E9%A2%91/P003_Chapter%2001_Exercise%20C%20Listening.mp3",
+picUrl: "../../images/sbs1e.jpg",
+name: "P003_Chapter 01_Exercise C Listening ",
+artists:"SBS" ,
+album:"国际朗文英语第1册练习"
+},
+
+};
+
+
+
+var story3 = 
 {
 
 "100001": {
 mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E4%B8%87%E4%BA%8B%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
 picUrl: "../../images/disk1.png",
 name: "英语万事小故事",
-artists:"Susan英语学堂" 
+artists:"Susan英语学堂" ,
+album:"英语小故事（3年级）"
 },
 
 "100002": {
 mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E4%BA%BA%E7%89%A9%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
 picUrl: "../../images/disk1.png",
 name: "英语人物小故事",
-artists:"Susan英语学堂" 
+artists:"Susan英语学堂" ,
+album:"英语小故事（3年级）"
 },
 
 "100003": {
 mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B/%E8%8B%B1%E8%AF%AD%E5%8A%A8%E7%89%A9%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
 picUrl: "../../images/disk1.png",
 name: "英语动物小故事",
-artists:"Susan英语学堂" 
+artists:"Susan英语学堂" ,
+album:"英语小故事（3年级）"
 },
 
 "100004": {
@@ -29,21 +117,68 @@ mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6
 picUrl: "../../images/disk1.png",
 name: "英语哲理小故事",
 artists:"Susan英语学堂" , 
- name: "英语哲理小故事"
+name: "英语哲理小故事",
+album:"英语小故事（3年级）"
 },
 
+};
 
+var story4 = 
+{
+
+"100001": {
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B%EF%BC%884%E5%B9%B4%E7%BA%A7%EF%BC%89/%E8%8B%B1%E8%AF%AD%E4%BA%BA%E7%89%A9%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语人物小故事",
+artists:"Susan英语学堂" ,
+album:"英语小故事（4年级）"
+},
+
+"100002": {
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B%EF%BC%884%E5%B9%B4%E7%BA%A7%EF%BC%89/%E8%8B%B1%E8%AF%AD%E5%AE%B6%E5%BA%AD%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语家庭小故事",
+artists:"Susan英语学堂" ,
+album:"英语小故事（4年级）"
+},
+
+"100003": {
+mp3Url:"http://susanlistening-1253455298.file.myqcloud.com/%E8%8B%B1%E8%AF%AD%E6%95%85%E4%BA%8B%EF%BC%884%E5%B9%B4%E7%BA%A7%EF%BC%89/%E8%8B%B1%E8%AF%AD%E6%9C%BA%E6%99%BA%E5%B0%8F%E6%95%85%E4%BA%8B.mp3",
+picUrl: "../../images/disk1.png",
+name: "英语机智小故事",
+artists:"Susan英语学堂" ,
+album:"英语小故事（4年级）"
+},
 
 
 };
 
 
 module.exports = {
-
-    'GET /api/sbs1': async (ctx, next) => {
+    'GET /api/books': async (ctx, next) => {
+        ctx.response.type = 'application/json';
+        ctx.response.body = books;
+    },
+    'GET /api/list/sbs1': async (ctx, next) => {
         ctx.response.type = 'application/json';
         ctx.response.body = sbs1;
     },
+    'GET /api/list/sbs1e': async (ctx, next) => {
+        ctx.response.type = 'application/json';
+        ctx.response.body = sbs1e;
+    },
+    'GET /api/list/story3': async (ctx, next) => {
+        ctx.response.type = 'application/json';
+        ctx.response.body = story3;
+    },
+    'GET /api/list/story4': async (ctx, next) => {
+        ctx.response.type = 'application/json';
+        ctx.response.body = story4;
+    },
+
+
+
+
 
 
 };
